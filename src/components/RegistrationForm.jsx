7d4eba4aa429adc20f27d311c351343148769db5
Felipe Catania <felicatania@gmail.com>
2018-05-21 11:25:18 -0300

    add emailjs

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Form, Input, Tooltip, Icon, Button } from 'antd';
+import { send } from 'emailjs-com/dist/email.js';
+import { serviceID, templateID, userID } from '../emailjs-config';
 
 const { TextArea } = Input;
 const FormItem = Form.Item;
 
 class RegistrationForm extends React.Component {
-  state = {
-    confirmDirty: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      confirmDirty: false
+    }
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        send(serviceID, templateID, values, userID)
+        .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          // mail succeeded
+        }, function(error) {
+          console.log('FAILED...', error);
+          // mail failed
+       });
       }
     });
   }
