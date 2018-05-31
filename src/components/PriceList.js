ecdbd0a641fa310d71fc38fa2d590efaedcdc136
Franco Catania <catania.fco@gmail.com>
2018-05-31 11:06:42 -0300

    text changes

--- src/components/PriceList.js
+++ src/components/PriceList.js
@@ -17,12 +17,13 @@ class PriceList extends Component {
             <h1 className="u-hidden">Camionetas desde Ezeiza</h1>
             <div className="priceItem__destinationContainer">
               <h2 className="priceItem__destination">Ezeiza</h2>
-              <h2 className="priceItem__description">Traslados desde y hasta</h2>
+              <h2 className="priceItem__description">Traslados desde o hasta</h2>
+              <h2 className="priceItem__description">Peajes incluidos</h2>
             </div>
             <div className="separatorLine" />
             <div className="priceItem__priceContainer">
               <span className="priceItem__currency">AR$</span>
-              <span className="priceItem__priceNumber">1.600</span>
+              <span className="priceItem__priceNumber">1.400</span>
             </div>
           </div>
 
@@ -30,12 +31,13 @@ class PriceList extends Component {
             <h1 className="u-hidden">Traslados a Aeroparque</h1>
             <div className="priceItem__destinationContainer">
               <h2 className="priceItem__destination">Aeroparque</h2>
-              <h2 className="priceItem__description">Traslados desde y hasta</h2>
+              <h2 className="priceItem__description">Traslados desde o hasta</h2>
+              <h2 className="priceItem__description">Peajes incluidos</h2>
             </div>
             <div className="separatorLine" />
             <div className="priceItem__priceContainer">
               <span className="priceItem__currency">AR$</span>
-              <span className="priceItem__priceNumber">1.300</span>
+              <span className="priceItem__priceNumber">1.000</span>
             </div>
           </div>
 
@@ -43,13 +45,13 @@ class PriceList extends Component {
             <h1 className="u-hidden">City Tour Privado</h1>
             <div className="priceItem__destinationContainer">
               <h2 className="priceItem__destination">City Tour</h2>
-              <h2 className="priceItem__description">City Tour privado con guía</h2>
+              <h2 className="priceItem__description">City Tour privado por hora</h2>
             </div>
             <div className="separatorLine" />
             <div className="priceItem__priceContainer">
               <span className="priceItem__currency">AR$</span>
-              <span className="priceItem__priceNumber">900</span>
-              <span className="priceItem__currency">/h</span>
+              <span className="priceItem__priceNumber">550</span>
+              <span className="priceItem__currency">/hora</span>
             </div>
           </div>
         </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ class Title extends Component {
     return (
       <div className="titleContainer titleContainer--animated">
         <h1 className="titleContainer__titleText">Traslados in/out Ezeiza y Aeroparque</h1>
-        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta 8 pasajeros, ofrecemos la alternativa perfecta para viajes familiar y coroporativos.</h2>
+        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta 7 pasajeros, ofrecemos la alternativa perfecta para viajes familiares y coroporativos. Consulte por todos nuestros servicios.</h2>
       </div>
     );
   }
