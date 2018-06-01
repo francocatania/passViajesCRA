63fcd1793e4bac0e60d2b717b017d66fba2ee3e4
Franco Catania <catania.fco@gmail.com>
2018-06-01 12:46:30 -0300

    Text correction

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ class Title extends Component {
     return (
       <div className="titleContainer titleContainer--animated">
         <h1 className="titleContainer__titleText">Traslados in/out Ezeiza y Aeroparque</h1>
-        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta <span className="titleContainer__relevant-text">7 pasajeros</span>, ofrecemos la alternativa perfecta para viajes familiares y coroporativos. Consulte por todos nuestros servicios.</h2>
+        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta <span className="titleContainer__relevant-text">7 pasajeros</span>, ofrecemos la alternativa perfecta para viajes familiares y corporativos. Consulte por todos nuestros servicios.</h2>
       </div>
     );
   }
