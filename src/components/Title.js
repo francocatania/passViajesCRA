15554ff3e32bbd8579bac7c08e5ec239adcd7d2b
Franco Catania <catania.fco@gmail.com>
2018-06-01 14:25:47 -0300

    responsive text fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+const windowWidth = window.innerWidth;
+
 
 class Title extends Component {
   render() {
     return (
       <div className="titleContainer titleContainer--animated">
         <h1 className="titleContainer__titleText">Traslados in/out Ezeiza y Aeroparque</h1>
-        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta <span className="titleContainer__relevant-text">7 pasajeros</span>, ofrecemos la alternativa perfecta para viajes familiares y corporativos. Consulte por todos nuestros servicios.</h2>
+        <h2 className="titleContainer__descriptionText">Servicio de transporte privado para CABA y Gran Buenos Aires. Con una capacidad de hasta {windowWidth > 1868 ? <br /> : ''}<span className="titleContainer__relevant-text">7 pasajeros</span>, ofrecemos la alternativa perfecta para viajes familiares y corporativos. Consulte por todos nuestros servicios.</h2>
       </div>
     );
   }
